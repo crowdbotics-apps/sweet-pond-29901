67a1d4c972c9afe385cdd53aca6838bb93b0e708
Crowdbotics <team@crowdbotics.com>
2021-08-18 02:58:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sweet_pond_29901Identifier",
+  url: "https://sweet_pond_29901Identifier.botics.co",
+  api: "https://sweet_pond_29901Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
